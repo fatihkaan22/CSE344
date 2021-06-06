02445130a6a82e9b41438c6736158cace5946924
fatihkaan22 <fatihkaan22@gmail.com>
2021-06-06 18:08:40 +0300

    parse query str

--- final/main.c
+++ final/main.c
@@ -1,12 +1,22 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-enum sql_commands { SELECT, SELECT_DISTINCT, UPDATE };
-enum sql_clause { DISTINCT, WHERE };
+enum sql_command { UNKNOWN = 0, SELECT, SELECT_DISTINCT, UPDATE };
+enum sql_keywords { WHERE, FROM };
 
 #define DELIM ","
+
+#define SET_IDX(idx, func)                                                     \
+  {                                                                            \
+    int n;                                                                     \
+    if ((n = (func)) == -1)                                                    \
+      return -1;                                                               \
+    else                                                                       \
+      idx += n;                                                                \
+  }
 
 #define R "\x1B[31m"
 #define C "\x1B[36m"
@@ -23,7 +33,20 @@ struct sql_table {
   size_t no_cols;
 };
 
+typedef struct pair {
+  char *key;
+  char *value;
+} pair;
+
 struct sql_table table;
+
+typedef struct query {
+  enum sql_command cmd;
+  bool distinct;
+  char *columns[128];
+  pair column_set[128];
+  pair column_filter[128];
+} query;
 
 char *csv_tokenizer(char *str, char **rest) {
   if (str == NULL) {
@@ -64,6 +87,8 @@ void print_row(record r) {
   }
   puts("");
 }
+
+bool query_end(char *s) { return (*s == ';'); }
 
 void read_csv(FILE *fp) {
 
@@ -113,7 +138,230 @@ void read_csv(FILE *fp) {
     }
     table.size++;
   }
+}
 
+// check string equality
+bool streq(char *s1, char *s2) {
+  char *t1 = strdup(s1);
+  char *t2 = strdup(s2);
+  char *p1 = t1;
+  char *p2 = t2;
+  for (; *p1; ++p1)
+    *p1 = tolower(*p1);
+  for (; *p2; ++p2)
+    *p2 = tolower(*p2);
+  bool res = (strcmp(t1, t2) == 0);
+  free(t1);
+  free(t2);
+  return res;
+}
+
+int read_cols(char *tokens[], char *columns[], char *until) {
+  int i = 0;
+  while (tokens[i] && !streq(tokens[i], until)) {
+    columns[i] = tokens[i];
+    ++i;
+  }
+  columns[i] = NULL;
+  if (tokens[i] == NULL || i == 0)
+    return -1;
+  return i;
+}
+
+int keyword_match(char *tokens[], char *s1) {
+  if (tokens[0] && streq(tokens[0], s1))
+    return 1;
+  fprintf(stderr, "ERROR: Couldn't parse query, %s was expected.\n", s1);
+  return -1;
+}
+
+/* int keyword_match2(char *tokens[], char *s1, char *s2) { */
+/*   if (tokens[0] && tokens[1] && streq(tokens[0], s1) && streq(tokens[1], s2))
+ */
+/*     return 2; */
+/*   fprintf(stderr, "ERROR: Couldn't parse query, %s %s was expected.\n", s1,
+ * s2); */
+/*   return -1; */
+/* } */
+
+/* int is_from_table(char *tokens[]) { */
+/*   return keyword_match2(tokens, "FROM", "TABLE"); */
+/* } */
+
+/* int is_table_set(char *tokens[]) { */
+/*   return keyword_match2(tokens, "TABLE", "SET"); */
+/* } */
+
+// returns number of tokens read
+int read_pairs(char *tokens[], pair column_filter[]) {
+  int i = 0;
+  while (tokens[i] && !(streq(tokens[i], "WHERE") || streq(tokens[i], ";"))) {
+    /* printf(">>%s\n", tokens[i]); */
+    if (streq(tokens[i], "*")) {
+      printf("YILDIZ\n");
+      column_filter[i].key = tokens[i];
+      return 1;
+    }
+    // TODO: malloc
+    // TODO: think about empty spaces
+    char *key = strtok(tokens[i], "=");
+    char *val = strtok(NULL, "='");
+    /* printf("%s\n", key); */
+    /* printf("%s\n", val); */
+    column_filter[i].key = key;
+    column_filter[i].value = val;
+    ++i;
+  }
+  column_filter[i].key = NULL;
+  if (tokens[i] == NULL || i == 0)
+    return -1;
+  return i;
+}
+
+// returns the index after consumed tokens
+int which_command(char *tokens[], enum sql_command *command) {
+  if (streq(tokens[0], "select")) {
+    if (streq(tokens[1], "distinct")) {
+      *command = SELECT_DISTINCT;
+      return 2;
+    } else {
+      *command = SELECT;
+      return 1;
+    }
+  }
+  if (streq(tokens[0], "update")) {
+    *command = UPDATE;
+    return 1;
+  }
+  fprintf(stderr, "Unknown command: %s\n", tokens[0]);
+  return -1;
+}
+
+void print_query(query q) {
+  puts("---QUERY---");
+  switch (q.cmd) {
+  case SELECT:
+    printf("SELECT\n");
+    break;
+  case UPDATE:
+    printf("UPDATE\n");
+    break;
+  case SELECT_DISTINCT:
+    printf("SELECT_DISTINCT\n");
+  }
+  printf("columns: ");
+  for (int i = 0; q.columns[i] != NULL; ++i) {
+    printf(" %s ", q.columns[i]);
+  }
+  printf("\ncolumn_set: ");
+  for (int i = 0; q.column_set[i].key != NULL; ++i) {
+    printf(" %s=", q.column_set[i].key);
+    printf("%s ", q.column_set[i].value);
+  }
+  printf("\ncolumn_filter: ");
+  for (int i = 0; q.column_filter[i].key != NULL; ++i) {
+    printf(" %s=", q.column_filter[i].key);
+    printf("%s ", q.column_filter[i].value);
+  }
+
+  puts("\n-----------");
+}
+
+int parse_query(char *query_str, query *q) {
+  char delims[] = " \t\r\n ,;";
+  char *tokens[64]; // WARN:
+  if (query_str[strlen(query_str) - 1] != ';') {
+    fprintf(stderr, "ERROR: Query doesn't ends with ';'\n");
+    return -1;
+  }
+  char *token = strtok(query_str, delims);
+  int no_tokens = 0;
+  // TODO: consider whitespaces before/after =
+  while (token) {
+    tokens[no_tokens++] = token;
+    token = strtok(NULL, delims);
+  }
+  tokens[no_tokens++] = ";";
+  tokens[no_tokens] = NULL;
+
+  int tok_idx = 0;
+  SET_IDX(tok_idx, which_command(tokens, &(q->cmd)));
+
+  switch (q->cmd) {
+  case SELECT_DISTINCT:
+    q->distinct = true;
+  case SELECT: {
+    SET_IDX(tok_idx, read_cols(tokens + tok_idx, q->columns, "FROM"));
+    SET_IDX(tok_idx, keyword_match(tokens + tok_idx, "FROM"));
+    SET_IDX(tok_idx, keyword_match(tokens + tok_idx, "TABLE"));
+    break;
+  }
+  case UPDATE: {
+    SET_IDX(tok_idx, keyword_match(tokens + tok_idx, "TABLE"));
+    SET_IDX(tok_idx, keyword_match(tokens + tok_idx, "SET"));
+    SET_IDX(tok_idx, read_pairs(tokens + tok_idx, q->column_set));
+    break;
+  }
+  default:
+    puts("ERROR"); // TODO:
+  }
+  if (query_end(tokens[tok_idx]))
+    return 0;
+
+  SET_IDX(tok_idx, keyword_match(tokens + tok_idx, "WHERE"));
+  SET_IDX(tok_idx, read_pairs(tokens + tok_idx, q->column_filter));
+  SET_IDX(tok_idx, keyword_match(tokens + tok_idx, ";"));
+  return 0;
+}
+
+int col_idx(char *str) {
+  for (size_t i = 0; i < table.no_cols; ++i) {
+    if (strcmp(table.header[i], str) == 0) {
+      return i;
+    }
+  }
+  fprintf(stderr, "ERROR: not in header\n");
+  return -1;
+}
+
+bool record_match(int row, pair *column_filter) {
+  /* print_row(table.records[row]); */
+  for (int i = 0; column_filter[i].key; ++i) {
+    int col = col_idx(column_filter[i].key);
+    if (col == -1)
+      return false;
+    if (table.records[row][col] &&
+        strcmp(table.records[row][col], column_filter[i].value) != 0)
+      return false;
+  }
+  return true;
+}
+
+int run_query(query q) {
+  switch (q.cmd) {
+  case SELECT: {
+    int result_record[512]; // TODO:
+    int res_size = 0;
+
+    for (size_t i = 0; i < table.size; ++i) {
+      if (record_match(i, q.column_filter)) {
+        result_record[res_size++] = i;
+      }
+    }
+    for (int i = 0; i < res_size; ++i) {
+      print_row(table.records[result_record[i]]);
+    }
+
+    break;
+  }
+  case UPDATE:
+    break;
+  case SELECT_DISTINCT:
+    break;
+  default:
+    puts("ERROR"); // TODO:
+  }
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -127,16 +375,37 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Cannot open file");
     exit(EXIT_FAILURE);
   }
-  /* read_csv(fp); */
+  read_csv(fp);
 
-  /* // print table */
-  /* print_row(table.header); */
-  /* for (size_t i = 0; i < table.size; ++i) { */
-  /*   print_row(table.records[i]); */
-  /* } */
+  // print table
+  print_row(table.header);
+  for (size_t i = 0; i < table.size; ++i) {
+    print_row(table.records[i]);
+  }
 
+  char *cmd;
 
+  /* cmd = "SELECT * FROM TABLE;"; */
+  cmd = "SELECT * FROM TABLE WHERE Series_reference='BDCQ.SEA1AA';";
+  /* cmd = "SELECT columnName1, columnName2, columnName3 FROM TABLE;"; */
+  /* cmd = "UPDATE TABLE SET columnName1='value1', columnName2='value2' WHERE "
+   */
+  /*       "columnName='valueX';"; */
+  /* cmd = "UPDATE TABLE SET columnName1='value1';"; */
+  /* cmd = "SELECT DISTINCT columnName1,columnName2 FROM TABLE;"; */
 
+  char *query_str = malloc(256);
+  strcpy(query_str, cmd);
+
+  query q;
+  memset(&q, 0, sizeof(q));
+  if (parse_query(query_str, &q) == -1) {
+    fprintf(stderr, "Error while parsing query: '%s'\n", cmd);
+  }
+
+  print_query(q);
+
+  run_query(q);
 
   return 0;
 }
